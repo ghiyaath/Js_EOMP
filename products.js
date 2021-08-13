ac8781bd8b38d9f3f4e6c8e5f0ecd20711e9d45f
ghiyaath <ghiyaathwilliams2@gmail.com>
2021-08-13 16:27:47 +0200

    added

--- products.js
+++ products.js
@@ -1,61 +1,27 @@
-// Script to initialize all 3rd party plugins, as well as needed functions for the site
+let signInButton = document.querySelector(".login-section");
 
-// Initialize fullpage Fullpage
-new fullpage("#fullpage", {
-    anchors: ["home", "products", "reviews", "contact"],
-    navigation: true,
-    navigationPosition: "left",
-    navigationTooltips: ["Home", "Products", "Reviews", "Contact"],
-    center: true,
-  });
-  
-  //   Init Animate On Scroll
-  AOS.init();
-  
-  $(document).ready(function () {
-    //   Initialize product slider and set options
-    $("#product-slider").owlCarousel({
-      items: 1,
-      margin: 0,
-      stagePadding: 200,
-      loop: true,
-      dots: false,
-      //   autoplay: true,
-      center: true,
-    });
-  });
-  
-  //   function to open modal
-  function openModal() {
-    document.getElementById("modal").classList.toggle("modal-active");
-  }
-  
-  //   Functionality to get random users
-  // Store URL in variable
-  let randomUserURL = "https://randomuser.me/api/?results=6";
-  
-  //   Fetch the data
-  fetch(randomUserURL)
-    .then((res) => res.json()) // Turn response into JSON format
+function login(username, password) {
+  console.log(username);
+  console.log(password);
+  fetch("https://protected-escarpment-66140.herokuapp.com/auth", {
+    method: "POST",
+    body: JSON.stringify({
+      username: `${username}`,
+      password: `${password}`,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
     .then((data) => {
-      // Get results from data and store in variable
-      let users = data.results;
-      // Get testimonial container from HTML
-      let testimonialContainer = document.getElementById("testimonial-slider");
-      // Loop over users and create testimonial for them
-      users.forEach((user) => {
-        testimonialContainer.innerHTML += `
-                  <div class="testimonial">
-                      <q class="testimonial-message">Lorem ipsum dolor sit amet consectetur adipisicing elit.</q>
-                      <img class="testimonial-img" src="${user.picture.large}" alt="${user.name.first} ${user.name.last}">
-                      <h4 class="testimonial-name">${user.name.first} ${user.name.last}</h4>
-                  </div>
-              `;
-      });
-      //   Initialize testimonial slider
-      $("#testimonial-slider").owlCarousel({
-        loop: true,
-        autoplay: true,
-        margin: 30,
-      });
+      if (data["access_token"]) {
+        console.log(data);
+        myStorage = window.localStorage;
+        myStorage.setItem("jwt-token", data["access_token"]);
+        myStorage.setItem("username", username);
+        myStorage.setItem("password", password);
+        window.location.href = "/product.html";
+      }
     });
+}
